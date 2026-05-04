cdfbe4a117d989a5dd64bac475b966fe19ef00ce
26RK <8ao.exe@gmail.com>
2026-05-04 13:11:51 +0200

    Update car_fly.lua

--- car_fly.lua
+++ car_fly.lua
@@ -8,7 +8,6 @@ local Player = game.Players.LocalPlayer
 
 local carFlyActive = false
 local flySpeed = 150
-
 local currentCarFlyKeybind = Enum.KeyCode.LeftAlt
 
 local hb, vl
@@ -22,48 +21,61 @@ local function getVehicleCollisionPart()
 	if not seat then return nil end
 	local vehicle = seat.Parent
 	if not vehicle or not vehicle:FindFirstChild("Body") then return nil end
-	local col = vehicle.Body:FindFirstChild("CollisionPart")
-	return col
+	return vehicle.Body:FindFirstChild("CollisionPart")
 end
 
 local function cleanupFly()
-	if hb then hb:Disconnect() hb = nil end
-	if vl then pcall(function() vl:Destroy() end) vl = nil end
+	if hb then 
+		hb:Disconnect() 
+		hb = nil 
+	end
+	if vl then 
+		pcall(function() vl:Destroy() end) 
+		vl = nil 
+	end
 end
 
-local function startFly()
+local function attach()
 	local col = getVehicleCollisionPart()
-	if not col then
-		WindUI:Notify({
-			Title = "Car Fly",
-			Content = "You must be in a vehicle!",
-			Duration = 2,
-		})
-		carFlyActive = false
-		return
-	end
-
-	cleanupFly()
-
-	local h = Player.Character:FindFirstChildOfClass("Humanoid")
-
+	if not col then return end
+	
+	if vl then pcall(function() vl:Destroy() end) end
+	
 	vl = Instance.new("BodyVelocity", col)
 	vl.MaxForce = Vector3.new(9e9, 9e9, 9e9)
 	vl.P = 9e4
 	vl.Velocity = Vector3.new()
-
-	hb = RunService.Heartbeat:Connect(function()
-		if not carFlyActive then if vl then vl.Velocity = Vector3.new() end return end
-		if not h.Sit then cleanupFly() carFlyActive = false return end
-		local t = Vector3.new()
-		local cam = workspace.CurrentCamera.CFrame
-		if UIS:IsKeyDown(Enum.KeyCode.W) then t += cam.LookVector * flySpeed end
-		if UIS:IsKeyDown(Enum.KeyCode.S) then t += cam.LookVector * -flySpeed end
-		if UIS:IsKeyDown(Enum.KeyCode.A) then t += cam.RightVector * -flySpeed end
-		if UIS:IsKeyDown(Enum.KeyCode.D) then t += cam.RightVector * flySpeed end
-		if vl then vl.Velocity = t end
-	end)
-
+	
+	if not hb then
+		hb = RunService.Heartbeat:Connect(function()
+			if not carFlyActive then
+				if vl then vl.Velocity = Vector3.new() end
+				return
+			end
+			
+			local col = getVehicleCollisionPart()
+			if not col then
+				carFlyActive = false
+				return
+			end
+			
+			local h = Player.Character and Player.Character:FindFirstChildOfClass("Humanoid")
+			if not h or not h.Sit then
+				carFlyActive = false
+				return
+			end
+			
+			local t = Vector3.new()
+			local cam = workspace.CurrentCamera.CFrame
+			if UIS:IsKeyDown(Enum.KeyCode.W) then t += cam.LookVector * flySpeed end
+			if UIS:IsKeyDown(Enum.KeyCode.S) then t += cam.LookVector * -flySpeed end
+			if UIS:IsKeyDown(Enum.KeyCode.A) then t += cam.RightVector * -flySpeed end
+			if UIS:IsKeyDown(Enum.KeyCode.D) then t += cam.RightVector * flySpeed end
+			
+			if vl then vl.Velocity = t end
+		end)
+	end
+	
 	WindUI:Notify({
 		Title = "Car Fly",
 		Content = "Car fly enabled! Use WASD to move.",
@@ -80,7 +92,7 @@ local carFlyToggle = Tabs.VehicleMods:Toggle({
 	Callback = function(state)
 		carFlyActive = state
 		if state then
-			startFly()
+			attach()
 		else
 			cleanupFly()
 		end
@@ -120,7 +132,7 @@ task.spawn(function()
 			carFlyActive = not carFlyActive
 			carFlyToggle:SetValue(carFlyActive)
 			if carFlyActive then
-				startFly()
+				attach()
 			else
 				cleanupFly()
 			end
