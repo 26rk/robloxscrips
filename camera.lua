0fcd215bd53c6f4442f7fd6272becf4a42467c66
26RK <8ao.exe@gmail.com>
2026-05-07 16:00:02 +0200

    Update camera.lua

--- camera.lua
+++ camera.lua
@@ -1,28 +1,23 @@
 repeat task.wait() until game:IsLoaded()
 
-local Camera = workspace.CurrentCamera
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 
 local player = Players.LocalPlayer
 
 player.CameraMaxZoomDistance = 10000
 player.CameraMinZoomDistance = 0.5
 
-task.spawn(function()
-    while true do
-        task.wait()
-        if Camera.CameraType == Enum.CameraType.Custom then
-            Camera.CameraSubject = player.Character and player.Character:FindFirstChild("Humanoid") or nil
-        end
-    end
-end)
-
-local function applyNoclip()
+local function noclipCamera()
     pcall(function()
-        if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-            player.Character.HumanoidRootPart.CanCollide = false
+        if player.Character then
+            for _, part in pairs(player.Character:GetDescendants()) do
+                if part:IsA("BasePart") then
+                    part.CanCollide = false
+                end
+            end
         end
     end)
 end
 
-game:GetService("RunService").RenderStepped:Connect(applyNoclip)
+RunService.RenderStepped:Connect(noclipCamera)
